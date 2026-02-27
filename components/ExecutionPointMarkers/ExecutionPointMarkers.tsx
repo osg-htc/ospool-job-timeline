ff5293a5f009bbacbc56ba54369c7ee65f170eea
clock <clock@wisc.edu>
2026-02-27 10:09:38 -0600

    Color by Run

--- components/ExecutionPointMarkers/ExecutionPointMarkers.tsx
+++ components/ExecutionPointMarkers/ExecutionPointMarkers.tsx
@@ -18,9 +18,9 @@ const ExecutionPointMarkers = ({time, jobResources}: ExecutionPointMarkersProps)
     <>
       {Object.values(jobResources).map((r) => {
 
-        const height = r.jobs.filter((j: JobRecord) => time > j.CompletionDate).length;
+        const jobsRan = r.jobs.filter((j: JobRecord) => time > j.CompletionDate);
         // compute a pixel height so the svg is visible even when `height` is 0
-        const markerHeight = height * 2
+        const markerHeight = 2
 
         return (
           <Marker
@@ -32,14 +32,19 @@ const ExecutionPointMarkers = ({time, jobResources}: ExecutionPointMarkersProps)
             {/* SVG height is driven by markerHeight so it scales with `height` */}
             <svg
               width={24}
-              height={markerHeight}
-              viewBox={`0 0 24 ${markerHeight}`}
+              height={markerHeight * jobsRan.length}
+              viewBox={`0 0 24 ${markerHeight * jobsRan.length}`}
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
               style={{display: 'block'}}
             >
-              {/* single rectangle whose height equals markerHeight and is visible */}
-              <rect x={8} y={0} width={8} height={markerHeight} fill={stringToColor(r.name)} rx={2} />
+              {
+                jobsRan.length > 0 && (
+                  jobsRan.map((j: JobRecord, i) => (
+                    <rect key={j.GlobalJobId} x={8} y={(jobsRan.length - i) * markerHeight} width={8} height={markerHeight} fill={stringToColor(j.RunId)} rx={2} />
+                  ))
+                )
+              }
             </svg>
           </Marker>
         )
